91442daa9da7847e23ac2d3e1d4f6a744bd4f860
Ameen Khan <mak_24x7@hotmail.co.uk>
2022-08-01 19:41:12 -0400

    implemented strings.isUnique soln

---
+++ src/com/akpg/strings/isUnique.java
@@ -0,0 +1,98 @@
+package com.akpg.strings;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Objects;
+
+public class isUnique {
+    /**
+     * Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional
+     * data structures
+     */
+
+    public static void main(String[] args) {
+        isUnique testClass = new isUnique();
+
+        System.out.println(testClass.isUnique(""));
+        System.out.println(testClass.isUnique("abc"));
+        System.out.println(testClass.isUnique("abca"));
+
+        System.out.println("\n");
+
+        System.out.println(testClass.isUniqueNoDS(""));
+        System.out.println(testClass.isUniqueNoDS("abc"));
+        System.out.println(testClass.isUniqueNoDS("abca"));
+
+        System.out.println("\n");
+
+        System.out.println(testClass.isUniqueNoDsSorted(""));
+        System.out.println(testClass.isUniqueNoDsSorted("abc"));
+        System.out.println(testClass.isUniqueNoDsSorted("abca"));
+    }
+
+    /**
+     * Using hashset
+     *
+     * Runtime is O(n), at worst case we loop through everything, string to char array is O(n) time and space
+     * Space is O(n), at worst we go through entire string
+     */
+    private boolean isUnique(String str) {
+
+        if (str.isBlank()) {
+            return true;
+        }
+
+        HashSet<Character> set = new HashSet<>();
+
+        for (Character c : str.toCharArray()) {
+            if (set.contains(c)) {
+                return false;
+            } else {
+                set.add(c);
+            }
+        }
+        return true;
+    }
+
+    /**
+     * naive approach is to go check rest of array per character, which is O(n^2) runtime but O(1) space
+     *
+     * or sort the string alphabetically, O(n log(n)) and check if any two consecutive letters are the same
+     *
+     */
+    private boolean isUniqueNoDS(String str) {
+        if (str.isBlank()) {
+            return true;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            Character currChar = str.charAt(i);
+
+            for (int k = i + 1; k < str.length(); k++) {
+                if (currChar.equals(str.charAt(k))) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isUniqueNoDsSorted(String str) {
+
+        if (str.isBlank()) {
+            return true;
+        }
+
+        char[] charArr = str.toCharArray();
+        Arrays.sort(charArr);
+
+        for (int i = 0; i < (charArr.length - 1); i++) {
+            if (Objects.equals(charArr[i], charArr[i + 1])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
